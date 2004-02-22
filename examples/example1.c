43eed448da9e32f479e5d05bb62d6692bfe9fdfa
David Schleef <ds@schleef.org>
2004-02-22 19:13:10 +0000

    added

---
+++ examples/example1.c
@@ -0,0 +1,31 @@
+
+#include <liboil/liboil.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char *argv[])
+{
+  double *dest;
+  int32_t *src;
+  int i;
+
+  oil_init();
+
+  src = malloc(100*sizeof(int32_t));
+  dest = malloc(100*sizeof(double));
+
+  for(i=0;i<100;i++){
+    src[i] = i;
+  }
+
+  conv_f64_s32 (dest, sizeof(double), src, sizeof(int32_t), 100);
+
+  for(i=0;i<100;i++){
+    printf("%d %g\n",src[i],dest[i]);
+  }
+
+  return 0;
+}
+
