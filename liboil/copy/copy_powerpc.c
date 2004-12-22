e9214f67e20ea9e735dd8391dd62d332e1378f90
David Schleef <ds@schleef.org>
2004-12-22 03:13:09 +0000

    * COPYING: change to BSD license * license_block: license block * liboil/copy/copy_powerpc.c: (copy_u8_altivec2), (copy_u8_altivec3): new implementation * testsuite/Makefile.am:

--- liboil/copy/copy_powerpc.c
+++ liboil/copy/copy_powerpc.c
@@ -66,3 +66,94 @@ OIL_DEFINE_IMPL_FULL (copy_u8_altivec, copy_u8, OIL_IMPL_REQUIRES_ALTIVEC);
 
 
 
+static void
+copy_u8_altivec2 (uint8_t *dest, uint8_t *src, int n)
+{
+  while((unsigned long)dest & 0xf) {
+    *dest++ = *src++;
+    n--;
+  }
+
+  if (n/16) {
+    asm volatile (
+        "  mtctr %2\n"
+        "  li %%r11, 0\n"
+        "  lvsl %%v2, 0, %1\n"
+        "  lvx %%v0, 0, %1\n"
+        "  addi %1, %1, 16\n"
+        "1:\n"
+#if 0
+        "  lvxl %%v1, %%r11, %1\n"
+        "  vperm %%v0, %%v0, %%v1, %%v2\n"
+        "  stvxl %%v0, %%r11, %0\n"
+        "  vor %%v0, %%v1, %%v1\n"
+        "  addi %%r11, %%r11, 16\n"
+#endif
+        "  stvx %%v0, %%r11, %0\n"
+        "  stvx %%v0, %%r11, %0\n"
+        "  stvx %%v0, %%r11, %0\n"
+        "  stvx %%v0, %%r11, %0\n"
+        "  bdnz+ 1b\n"
+        "  add %0, %0, %%r11\n"
+        "  add %1, %1, %%r11\n"
+        "  addi %1, %1, -16\n"
+      : "+b" (dest), "+b" (src)
+      : "r" (n/16));
+  }
+
+  n &= 0xf;
+  while(n) {
+    *dest++ = *src++;
+    n--;
+  }
+}
+OIL_DEFINE_IMPL_FULL (copy_u8_altivec2, copy_u8, OIL_IMPL_REQUIRES_ALTIVEC);
+
+
+
+
+static void
+copy_u8_altivec3 (uint8_t *dest, uint8_t *src, int n)
+{
+  while((unsigned long)dest & 0xf) {
+    *dest++ = *src++;
+    n--;
+  }
+
+  if (n/32) {
+    asm volatile (
+        "  mtctr %2\n"
+        "  li %%r11, 0\n"
+        "  lvsl %%v3, 0, %1\n"
+        "  lvx %%v0, 0, %1\n"
+        "  addi %1, %1, 16\n"
+        "1:\n"
+        "  lvxl %%v1, %%r11, %1\n"
+        "   lvxl %%v2, %%r11, %1\n"
+        "    lvxl %%v3, %%r11, %1\n"
+        "     lvxl %%v4, %%r11, %1\n"
+        "  stvxl %%v1, %%r11, %0\n"
+        "   stvxl %%v2, %%r11, %0\n"
+        "    stvxl %%v3, %%r11, %0\n"
+        "     stvxl %%v4, %%r11, %0\n"
+        "  bdnz+ 1b\n"
+        "  add %0, %0, %%r11\n"
+        "  add %1, %1, %%r11\n"
+        "  addi %1, %1, -16\n"
+      : "+b" (dest), "+b" (src)
+      : "r" (n/32));
+  }
+
+  n &= 0xf;
+  while(n) {
+    *dest++ = *src++;
+    n--;
+  }
+}
+OIL_DEFINE_IMPL_FULL (copy_u8_altivec3, copy_u8, OIL_IMPL_REQUIRES_ALTIVEC);
+
+
+
+
+
+
