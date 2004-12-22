8cb5170836fea0273ce855fafdca15aa0239b86a
David Schleef <ds@schleef.org>
2004-12-22 03:55:21 +0000

    Global relicensing. * liboil/build_prototypes.c: (print_header): * liboil/colorspace/rgb2bgr.c: * liboil/colorspace/rgb2bgr_powerpc.c: * liboil/colorspace/rgb2rgba.c: * liboil/colorspace/rgb2rgba_powerpc.c: * liboil/conv/conv.h: * liboil/conv/conv_3dnow.c: * liboil/conv/conv_bitstuff.c: * liboil/conv/conv_c.c: * liboil/conv/conv_misc.c: * liboil/conv/conv_powerpc.c: * liboil/conv/conv_ref.c: * liboil/conv/conv_sse.c: * liboil/copy/copy.c: * liboil/copy/copy_i386.c: * liboil/copy/copy_powerpc.c: * liboil/copy/permute.c: * liboil/copy/splat_ref.c: * liboil/copy/tablelookup_ref.c: * liboil/copy/trans8x8.c: * liboil/copy/trans8x8_i386.c: * liboil/dct/dct.h: * liboil/dct/dct12_f32.c: * liboil/dct/dct36.c: * liboil/dct/dct36_f32.c: * liboil/dct/fdct8_f64.c: * liboil/dct/fdct8x8_f64.c: * liboil/dct/fdct8x8s_s16.c: * liboil/dct/idct8_f64.c: * liboil/dct/idct8x8_c.c: * liboil/dct/imdct32_f32.c: * liboil/jpeg/convert8x8_c.c: * liboil/jpeg/jpeg.h: * liboil/jpeg/jpeg_rgb_decoder.c: * liboil/jpeg/quantize8x8_c.c: * liboil/jpeg/yuv2rgb_c.c: * liboil/jpeg/zigzag8x8_c.c: * liboil/jpeg/zigzag8x8_powerpc.c: * liboil/junk/conv8x8_f64_s16.c: * liboil/junk/conv8x8_s16x8_s8.c: * liboil/junk/conv_f64_s16.c: * liboil/junk/conv_s16_f64.c: * liboil/junk/downsample1x_f64.c: * liboil/junk/downsample2_f64.c: * liboil/junk/downsample2_s16.c: * liboil/junk/downsample2_u8.c: * liboil/junk/downsamplex_f64.c: * liboil/junk/fdct8x8_s16.c: * liboil/junk/get8x8_f64.c: * liboil/junk/idct8x8_f64.c: * liboil/junk/idct8x8_s16.c: * liboil/junk/idct8x8s_s16.c: * liboil/junk/trans8x8_f32.c: * liboil/junk/trans8x8_s16.c: * liboil/junk/zigzag8x8_s16.c: * liboil/liboil.h: * liboil/liboil_begin.c: * liboil/liboil_end.c: * liboil/liboilcpu.c: * liboil/liboilcpu.h: * liboil/liboildebug.c: * liboil/liboildebug.h: * liboil/liboilfuncs.h: * liboil/liboilfunction.c: * liboil/liboilfunction.h: * liboil/liboilparameter.h: * liboil/liboilprofile.c: * liboil/liboilprofile.h: * liboil/liboilprototype.c: * liboil/liboilprototype.h: * liboil/liboilrandom.h: * liboil/liboiltest.c: * liboil/liboiltest.h: * liboil/liboiltypes.h: * liboil/md5/md5.c: * liboil/md5/md5.h: * liboil/md5/md5_i386.c: * liboil/md5/md5_powerpc.c: * liboil/null.c: * liboil/simdpack/abs.c: * liboil/simdpack/abs_i386.c: * liboil/simdpack/abs_misc.c: * liboil/simdpack/abs_powerpc.c: * liboil/simdpack/average2_u8.c: * liboil/simdpack/clip_powerpc.c: * liboil/simdpack/clip_ref.c: * liboil/simdpack/clip_s16.c: * liboil/simdpack/clip_s32.c: * liboil/simdpack/diffsquaresum_f64.c: * liboil/simdpack/mix_powerpc.c: * liboil/simdpack/mix_u8.c: * liboil/simdpack/mult8x8_i386.c: * liboil/simdpack/mult8x8_s16.c: * liboil/simdpack/multsum.c: * liboil/simdpack/multsum_powerpc.c: * liboil/simdpack/sad8x8.c: * liboil/simdpack/sad8x8_powerpc.c: * liboil/simdpack/scalaradd.c: * liboil/simdpack/scalarmult.c: * liboil/simdpack/scalarmult_i386.c: * liboil/simdpack/simdpack.h: * liboil/simdpack/sincos_f64.c: * liboil/simdpack/squaresum_f64.c: * liboil/simdpack/sum_f64.c: * liboil/simdpack/vectoradd_f64.c:

--- liboil/dct/dct12_f32.c
+++ liboil/dct/dct12_f32.c
@@ -1,19 +1,28 @@
-/* liboil - Library of Optimized Inner Loops
- * Copyright (C) 2003  David A. Schleef <ds@schleef.org>
+/*
+ * LIBOIL - Library of Optimized Inner Loops
+ * Copyright (c) 2003,2004 David A. Schleef <ds@schleef.org>
+ * All rights reserved.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of version 2.1 of the GNU Lesser General
- * Public License as published by the Free Software Foundation.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
- * Boston, MA 02111-1307 USA.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 #ifdef HAVE_CONFIG_H
@@ -27,6 +36,7 @@
 
 OIL_DEFINE_CLASS (dct12_f32, "float *d_18, int dstr, float *s_18, int sstr");
 
+/* FIXME this appears to be derived from mpglib */
 static void
 dct12_f32_ref (float *dest, int dstr, float *src, int sstr)
 {
@@ -137,100 +147,4 @@ dct12_f32_ref1(float *dest, int dstr, float *src, int sstr)
 }
 OIL_DEFINE_IMPL (dct12_f32_ref1, dct12_f32);
 #endif
-
-/* copyright: from mpglib */
-/*
- * new DCT12
- */
-static void
-dct12_f32_mpglib(float *dest, int dstr, float *src, int sstr)
-{
-     float in0,in1,in2,in3,in4,in5;
-     float *out2 = dest;
-     float *in = src;
-     float wi[12];
-     int i;
-     float tmp0, tmp1, tmp2;
-
-     float COS6_2 = cos(M_PI / 6.0 * 2.0);
-     float COS6_1 = cos(M_PI / 6.0 * 1.0);
-     float tfcos12[3];
-
-     for(i=0;i<3;i++){
-	     tfcos12[i] = 0.5 / cos ( M_PI * (i*2.0+1.0) / 12.0 );
-     }
-     for(i=0;i<12;i++){
-	     wi[i] = 0.5 * sin( M_PI / 24.0 * (double) (2*i+1) ) / cos ( M_PI * (double) (2*i+7) / 24.0 );
-     }
-
-     out2[12]=out2[13]=out2[14]=out2[15]=out2[16]=out2[17]=0.0;
-
-     in5 = in[5*3] + in[4*3];
-     in4 = in[4*3] + in[3*3];
-     in3 = in[3*3] + in[2*3];
-     in2 = in[2*3] + in[1*3];
-     in1 = in[1*3] + in[0*3];
-     in0 = in[0*3];
-                            
-     //in5 += in3;
-     in5 = in[5*3] + in[4*3] + in[3*3] + in[2*3];
-     //in3 += in1;
-     in3 = in[3*3] + in[2*3] + in[1*3] + in[0*3];
-                            
-     //in2 *= COS6_1;
-     in2 = COS6_1 * (in[2*3] + in[1*3]);
-
-     //in3 *= COS6_1;
-     in3 = COS6_1 * (in[3*3] + in[2*3] + in[1*3] + in[0*3]);
-
-     //tmp1 = (in0 - in4);
-     tmp1 = in[0*3] - in[4*3] - in[3*3];
-
-     //tmp2 = (in1 - in5) * tfcos12[1];
-     tmp2 = tfcos12[1] * (in[1*3] + in[0*3] - in[5*3] - in[4*3] - in[3*3] - in[2*3]);
-
-     //tmp0 = tmp1 + tmp2;
-     tmp0 = (1 + tfcos12[1]) * (in[0*3] - in[4*3] - in[3*3]) +
-             tfcos12[1] * (in[1*3] - in[5*3] - in[2*3]);
-
-     //tmp1 -= tmp2;
-     tmp1 = (1 - tfcos12[1]) * (in[0*3] - in[4*3] - in[3*3]) -
-             tfcos12[1] * (in[1*3] - in[5*3] - in[2*3]);
-
-     out2[11-1] = tmp0 * wi[11-1];
-     out2[6 +1] = tmp0 * wi[6+1];
-     out2[0+1] += tmp1 * wi[1];
-     out2[5-1] += tmp1 * wi[5-1];
-
-     in0 += in4 * COS6_2;
-                         
-     in4 = in0 + in2;
-     in0 -= in2;
-                 
-     in1 += in5 * COS6_2;
-                        
-     in5 = (in1 + in3) * tfcos12[0];
-     in1 = (in1 - in3) * tfcos12[2];
-                       
-     in3 = in4 + in5;
-     in4 = in4 - in5;
-               
-     in2 = in0 + in1;
-     in0 = in0 - in1;
-
-     out2[11-0] = in2 * wi[11-0];
-     out2[6 +0] = in2 * wi[6+0];
-     out2[6 +2] = in3 * wi[6+2];
-     out2[11-2] = in3 * wi[11-2];
-
-     out2[0+0] += in0 * wi[0];
-     out2[5-0] += in0 * wi[5-0];
-     out2[0+2] += in4 * wi[2];
-     out2[5-2] += in4 * wi[5-2];
-}
-
-OIL_DEFINE_IMPL (dct12_f32_mpglib, dct12_f32);
-
-
-
 
--- liboil/md5/md5.h
+++ liboil/md5/md5.h
@@ -1,19 +1,28 @@
-/* liboil - Library of Optimized Inner Loops
- * Copyright (C) 2004  David A. Schleef <ds@schleef.org>
+/*
+ * LIBOIL - Library of Optimized Inner Loops
+ * Copyright (c) 2004 David A. Schleef <ds@schleef.org>
+ * All rights reserved.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of version 2.1 of the GNU Lesser General
- * Public License as published by the Free Software Foundation.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
- * Boston, MA 02111-1307 USA.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 #ifndef _LIBOIL_MD5_H_
