0fa5a5131ca8418ab2887ad14f00a34215067484
David Schleef <ds@schleef.org>
2005-01-10 04:58:38 +0000

    * autogen.sh: new and improved simple autogen.sh * configure.ac: oops, define libversion correctly

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 set -x
-aclocal -I m4
-libtoolize --force
-autoheader
-autoconf
-automake -a --foreign
-#./configure --enable-maintainer-mode --enable-alternate-optimization $@
+set -e
+autoreconf -i -f
 ./configure --enable-maintainer-mode --disable-static $@
