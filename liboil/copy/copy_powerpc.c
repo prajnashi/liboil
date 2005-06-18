cb5c9c5b48b447b3fc3c3d31072b9fc6b52b54fd
David Schleef <ds@schleef.org>
2005-06-18 01:07:34 +0000

    * liboil/copy/copy_powerpc.c: (copy_u8_altivec), (copy_u8_altivec2): Rewrite so that they actually work. * liboil/motovec/Makefile.am: Fix up motovec stuff * liboil/motovec/motovec.c: (copy_u8_motovec), (splat_u8_ns_motovec): same

--- liboil/copy/copy_powerpc.c
+++ liboil/copy/copy_powerpc.c
@@ -37,30 +37,46 @@ OIL_DECLARE_CLASS(copy_u8);
 static void
 copy_u8_altivec (uint8_t *dest, uint8_t *src, int n)
 {
-  while((unsigned long)dest & 0xf) {
+  while((unsigned long)dest & 0xf && n>0) {
     *dest++ = *src++;
     n--;
   }
 
   if (n/16) {
-    asm volatile (
-        "  mtctr %2\n"
-        "  li r11, 0\n"
-        "  lvsl v2, 0, %1\n"
-        "  lvx v0, 0, %1\n"
-        "  addi %1, %1, 16\n"
-        "1:\n"
-        "  lvxl v1, r11, %1\n"
-        "  vperm v0, v0, v1, v2\n"
-        "  stvxl v0, r11, %0\n"
-        "  vor v0, v1, v1\n"
-        "  addi r11, r11, 16\n"
-        "  bdnz+ 1b\n"
-        "  add %0, %0, r11\n"
-        "  add %1, %1, r11\n"
-        "  addi %1, %1, -16\n"
-      : "+b" (dest), "+b" (src)
-      : "r" (n/16));
+    if ((unsigned long)src & 0xf) {
+      asm volatile (
+          "  mtctr %2\n"
+          "  li r11, 0\n"
+          "  lvsl v2, 0, %1\n"
+          "  lvx v0, 0, %1\n"
+          "  addi %1, %1, 16\n"
+          "1:\n"
+          "  lvx v1, r11, %1\n"
+          "  vperm v0, v0, v1, v2\n"
+          "  stvx v0, r11, %0\n"
+          "  vor v0, v1, v1\n"
+          "  addi r11, r11, 16\n"
+          "  bdnz+ 1b\n"
+          "  add %0, %0, r11\n"
+          "  add %1, %1, r11\n"
+          "  addi %1, %1, -16\n"
+        : "+b" (dest), "+b" (src)
+        : "r" (n/16));
+    } else {
+      asm volatile (
+          "  mtctr %2\n"
+          "  li r11, 0\n"
+          "1:\n"
+          "  lvx v1, r11, %1\n"
+          "  stvx v1, r11, %0\n"
+          "  addi r11, r11, 16\n"
+          "  bdnz+ 1b\n"
+          "  add %0, %0, r11\n"
+          "  add %1, %1, r11\n"
+          "  addi %1, %1, -16\n"
+        : "+b" (dest), "+b" (src)
+        : "r" (n/16));
+    }
   }
 
   n &= 0xf;
@@ -71,98 +87,60 @@ copy_u8_altivec (uint8_t *dest, uint8_t *src, int n)
 }
 OIL_DEFINE_IMPL_FULL (copy_u8_altivec, copy_u8, OIL_IMPL_FLAG_ALTIVEC);
 
-
-
-
-
 static void
 copy_u8_altivec2 (uint8_t *dest, uint8_t *src, int n)
 {
-  while((unsigned long)dest & 0xf) {
+  while((unsigned long)dest & 0xf && n>0) {
     *dest++ = *src++;
     n--;
   }
 
-  if (n/16) {
+  if (n/64) {
     asm volatile (
         "  mtctr %2\n"
         "  li r11, 0\n"
-        "  lvsl v2, 0, %1\n"
+        "  lvsl v5, 0, %1\n"
         "  lvx v0, 0, %1\n"
         "  addi %1, %1, 16\n"
         "1:\n"
-#if 0
-        "  lvxl v1, r11, %1\n"
-        "  vperm v0, v0, v1, v2\n"
-        "  stvxl v0, r11, %0\n"
-        "  vor v0, v1, v1\n"
+        "  lvx v1, r11, %1\n"
         "  addi r11, r11, 16\n"
-#endif
+        "  lvx v2, r11, %1\n"
+        "  addi r11, r11, 16\n"
+        "  lvx v3, r11, %1\n"
+        "  addi r11, r11, 16\n"
+        "  lvx v4, r11, %1\n"
+        "  addi r11, r11, -48\n"
+
+        "  vperm v0, v0, v1, v5\n"
+        "  vperm v1, v1, v2, v5\n"
+        "  vperm v2, v2, v3, v5\n"
+        "  vperm v3, v3, v4, v5\n"
+
         "  stvx v0, r11, %0\n"
-        "  stvx v0, r11, %0\n"
-        "  stvx v0, r11, %0\n"
-        "  stvx v0, r11, %0\n"
+        "  addi r11, r11, 16\n"
+        "  stvx v1, r11, %0\n"
+        "  addi r11, r11, 16\n"
+        "  stvx v2, r11, %0\n"
+        "  addi r11, r11, 16\n"
+        "  stvx v3, r11, %0\n"
+        "  addi r11, r11, 16\n"
+        "  vor v0, v4, v4\n"
+
         "  bdnz+ 1b\n"
         "  add %0, %0, r11\n"
         "  add %1, %1, r11\n"
         "  addi %1, %1, -16\n"
       : "+b" (dest), "+b" (src)
-      : "r" (n/16));
+      : "r" (n/64));
   }
 
-  n &= 0xf;
+  n &= 0x3f;
   while(n) {
     *dest++ = *src++;
     n--;
   }
 }
 OIL_DEFINE_IMPL_FULL (copy_u8_altivec2, copy_u8, OIL_IMPL_FLAG_ALTIVEC);
-
-
-
-
-static void
-copy_u8_altivec3 (uint8_t *dest, uint8_t *src, int n)
-{
-  while((unsigned long)dest & 0xf) {
-    *dest++ = *src++;
-    n--;
-  }
-
-  if (n/32) {
-    asm volatile (
-        "  mtctr %2\n"
-        "  li r11, 0\n"
-        "  lvsl v3, 0, %1\n"
-        "  lvx v0, 0, %1\n"
-        "  addi %1, %1, 16\n"
-        "1:\n"
-        "  lvxl v1, r11, %1\n"
-        "   lvxl v2, r11, %1\n"
-        "    lvxl v3, r11, %1\n"
-        "     lvxl v4, r11, %1\n"
-        "  stvxl v1, r11, %0\n"
-        "   stvxl v2, r11, %0\n"
-        "    stvxl v3, r11, %0\n"
-        "     stvxl v4, r11, %0\n"
-        "  bdnz+ 1b\n"
-        "  add %0, %0, r11\n"
-        "  add %1, %1, r11\n"
-        "  addi %1, %1, -16\n"
-      : "+b" (dest), "+b" (src)
-      : "r" (n/32));
-  }
-
-  n &= 0xf;
-  while(n) {
-    *dest++ = *src++;
-    n--;
-  }
-}
-OIL_DEFINE_IMPL_FULL (copy_u8_altivec3, copy_u8, OIL_IMPL_FLAG_ALTIVEC);
-
-
-
-
 
 
