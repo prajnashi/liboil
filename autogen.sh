33bdf27efddbfad85c5abf315e99ecdd60233d11
David Schleef <ds@schleef.org>
2005-07-26 20:26:21 +0000

    * CVS_HAS_MOVED: disable CVS, pointing to new location * autogen.sh: * configure.ac: * cvs-changeroot:

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
+
+cat <<-EOF
+***
+***  liboil CVS has moved.
+***
+***  The new project root is
+***  :pserver:anoncvs@anoncvs.freedesktop.org:/cvs/liboil
+***
+***  Use the script
+***  ./cvs-changeroot :pserver:anoncvs@anoncvs.freedesktop.org:/cvs/liboil
+***  to automatically change to the new repository.
+***
+EOF
+
+exit 0
+
 autoreconf -i -f &&
 ./configure --enable-maintainer-mode --disable-static $@
