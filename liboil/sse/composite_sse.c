1e853452d1c28919d2984b5eb5a148fba1a67a04
Eric Anholt <anholt@FreeBSD.org>
2005-12-21 01:27:54 +0000

    Add more SSE2-intrinsics composite code.  There are actually 2 copies, unless we can figure out if 2pix is always slower.  The 4pix version operates on registers with 4 packed pixels, and does unpacking for the purposes of the muldiv stage.  On the other hand, the 2pix version works on registers containing 2 unpacked pixels.  Speeds are similar, and generally around 25 to 50% the ticks of _ref, according to oil-inspect. It's not very pretty code, but I've been staring at it for too long and want to get it committed.

--- liboil/sse/composite_sse.c
+++ liboil/sse/composite_sse.c
@@ -30,12 +30,111 @@
 #include <liboil/liboilclasses.h>
 #include <liboil/liboilfunction.h>
 #include <emmintrin.h>
+#include <liboil/liboilcolorspace.h>
+
+#define COMPOSITE_ADD(d,s) oil_clamp_255((d) + (s))
 
 static void
-composite_add_u8_const_src_sse (uint8_t *dest, uint8_t *src1_1, int n)
+composite_add_argb_sse (uint32_t *dest, const uint32_t *src, int n)
 {
-  __m128i xmm1;
-  uint8_t val = *src1_1;
+  /* Initial operations to align the destination pointer */
+  for (; ((long)dest & 15) && (n > 0); n--) {
+    uint32_t d = *dest, s = *src++;
+
+    *dest++ = oil_argb(
+	COMPOSITE_ADD(oil_argb_A(d), oil_argb_A(s)),
+	COMPOSITE_ADD(oil_argb_R(d), oil_argb_R(s)),
+	COMPOSITE_ADD(oil_argb_G(d), oil_argb_G(s)),
+	COMPOSITE_ADD(oil_argb_B(d), oil_argb_B(s)));
+  }
+  for (; n >= 4; n -= 4) {
+    __m128i s;
+    s = _mm_loadu_si128((__m128i *)src);
+    *(__m128i *)dest = _mm_adds_epu8(s, *(__m128i *)dest);
+    src += 4;
+    dest += 4;
+  }
+  for (; n > 0; n--) {
+    uint32_t d = *dest, s = *src++;
+
+    *dest++ = oil_argb(
+	COMPOSITE_ADD(oil_argb_A(d), oil_argb_A(s)),
+	COMPOSITE_ADD(oil_argb_R(d), oil_argb_R(s)),
+	COMPOSITE_ADD(oil_argb_G(d), oil_argb_G(s)),
+	COMPOSITE_ADD(oil_argb_B(d), oil_argb_B(s)));
+  }
+}
+OIL_DEFINE_IMPL_FULL (composite_add_argb_sse, composite_add_argb,
+    OIL_IMPL_FLAG_SSE2);
+
+static void
+composite_add_argb_const_src_sse (uint32_t *dest, const uint32_t *src_1, int n)
+{
+  __m128i s;
+  uint32_t val = *src_1;
+
+  /* Initial operations to align the destination pointer */
+  for (; ((long)dest & 15) && (n > 0); n--) {
+    uint32_t d = *dest;
+
+    *dest++ = oil_argb(
+	COMPOSITE_ADD(oil_argb_A(d), oil_argb_A(val)),
+	COMPOSITE_ADD(oil_argb_R(d), oil_argb_R(val)),
+	COMPOSITE_ADD(oil_argb_G(d), oil_argb_G(val)),
+	COMPOSITE_ADD(oil_argb_B(d), oil_argb_B(val)));
+  }
+  s = _mm_set1_epi32(val);
+  for (; n >= 4; n -= 4) {
+    __m128i xmm0;
+    xmm0 = _mm_adds_epu8(s, *(__m128i *)dest);
+    _mm_store_si128((__m128i *)dest, xmm0);
+    dest += 4;
+  }
+  for (; n > 0; n--) {
+    uint32_t d = *dest;
+
+    *dest++ = oil_argb(
+	COMPOSITE_ADD(oil_argb_A(d), oil_argb_A(val)),
+	COMPOSITE_ADD(oil_argb_R(d), oil_argb_R(val)),
+	COMPOSITE_ADD(oil_argb_G(d), oil_argb_G(val)),
+	COMPOSITE_ADD(oil_argb_B(d), oil_argb_B(val)));
+  }
+}
+OIL_DEFINE_IMPL_FULL (composite_add_argb_const_src_sse,
+    composite_add_argb_const_src, OIL_IMPL_FLAG_SSE2);
+
+static void
+composite_add_u8_sse (uint8_t *dest, const uint8_t *src, int n)
+{
+  /* Initial operations to align the destination pointer */
+  for (; ((long)dest & 15) && (n > 0); n--) {
+    int x = *dest + *src++;
+    if (x > 255) 
+      x = 255;
+    *dest++ = x;
+  }
+  for (; n >= 16; n -= 16) {
+    __m128i d, s;
+    s = _mm_loadu_si128((__m128i *)src);
+    d = _mm_adds_epu8(s, *(__m128i *)dest);
+    _mm_store_si128((__m128i *)dest, d);
+    dest += 16;
+  }
+  for (; n > 0; n--) {
+    int x = *dest + *src++;
+    if (x > 255) 
+      x = 255;
+    *dest++ = x;
+  }
+}
+OIL_DEFINE_IMPL_FULL (composite_add_u8_sse, composite_add_u8,
+    OIL_IMPL_FLAG_SSE2);
+
+static void
+composite_add_u8_const_src_sse (uint8_t *dest, const uint8_t *src_1, int n)
+{
+  __m128i s;
+  uint8_t val = *src_1;
 
   /* Initial operations to align the destination pointer */
   for (; ((long)dest & 15) && (n > 0); n--) {
@@ -44,12 +143,11 @@ composite_add_u8_const_src_sse (uint8_t *dest, uint8_t *src1_1, int n)
       x = 255;
     *dest++ = x;
   }
-  xmm1 = _mm_set1_epi8(val);
+  s = _mm_set1_epi8(val);
   for (; n >= 16; n -= 16) {
-    __m128i xmm0;
-    xmm0 = _mm_load_si128((__m128i *)dest);
-    xmm0 = _mm_adds_epu8(xmm0, xmm1);
-    _mm_store_si128((__m128i *)dest, xmm0);
+    __m128i d;
+    d = _mm_adds_epu8(*(__m128i *)dest, s);
+    _mm_store_si128((__m128i *)dest, d);
     dest += 16;
   }
   for (; n > 0; n--) {
