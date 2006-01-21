49e692e38aefbe5ee2549b9cfc7327d9dd6e2bdd
David Schleef <ds@schleef.org>
2006-01-21 01:59:01 +0000

    * configure.ac: Add LIBOIL_OLD_MAJORMINOR * liboil/Makefile.am: Use LIBOIL_OLD_MAJORMINOR and link liboiltmp.c into liboiltmp, so that it doesn't have unresolved symbols. * liboil/liboiltmp.c: new

---
+++ liboil/liboiltmp.c
@@ -0,0 +1,9 @@
+
+/* A few stubs to get liboiltmp1 to link correctly. */
+
+void oil_param_get_source_data(void) {}
+void oil_random_alpha(void) {}
+void oil_random_argb(void) {}
+void oil_test_get_source_data(void) {}
+void oil_test_get_value(void) {}
+
