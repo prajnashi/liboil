f38029458f36490833fe2563f939b103319c7957
David Schleef <ds@ginger.bigkitten.com>
2008-03-13 13:53:18 -0700

    Fix scalarmultiply_f64_ns SSE code

--- liboil/sse/math_sse_unroll2.c
+++ liboil/sse/math_sse_unroll2.c
@@ -311,7 +311,6 @@ scalarmultiply_f32_ns_sse_unroll2 (float *dest, float *src1, float *val, int n)
 }
 OIL_DEFINE_IMPL_FULL (scalarmultiply_f32_ns_sse_unroll2, scalarmultiply_f32_ns, OIL_IMPL_FLAG_SSE);
 
-#ifdef ENABLE_BROKEN_IMPLS
 static void
 scalarmultiply_f64_ns_sse2_unroll2 (double *dest, double *src1, double *val, int n)
 {
@@ -322,21 +321,20 @@ scalarmultiply_f64_ns_sse2_unroll2 (double *dest, double *src1, double *val, int
     *dest++ = *src1++ * *val;
   }
   xmm1 = _mm_load_pd1(val);
-  for (; n >= 8; n -= 8) {
+  for (; n >= 4; n -= 4) {
     __m128d xmm0;
     xmm0 = _mm_loadu_pd(src1);
     xmm0 = _mm_mul_pd(xmm0, xmm1);
     _mm_store_pd(dest, xmm0);
-    xmm0 = _mm_loadu_pd(src1 + 4);
+    xmm0 = _mm_loadu_pd(src1 + 2);
     xmm0 = _mm_mul_pd(xmm0, xmm1);
-    _mm_store_pd(dest + 4, xmm0);
-    dest += 8;
-    src1 += 8;
+    _mm_store_pd(dest + 2, xmm0);
+    dest += 4;
+    src1 += 4;
   }
   for (; n > 0; n--) {
     *dest++ = *src1++ * *val;
   }
 }
 OIL_DEFINE_IMPL_FULL (scalarmultiply_f64_ns_sse2_unroll2, scalarmultiply_f64_ns, OIL_IMPL_FLAG_SSE2);
-#endif
 
