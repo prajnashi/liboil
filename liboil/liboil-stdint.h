8dc7aff61dec8f17b2898d57863b7d070cd4e10e
David Schleef <ds@ginger.bigkitten.com>
2008-05-03 15:25:36 -0700

    stdint.h is created by configure.  Remove from repository.

--- liboil/liboil-stdint.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LIBOIL_LIBOIL_LIBOIL_STDINT_H
-#define _LIBOIL_LIBOIL_LIBOIL_STDINT_H 1
-#ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "liboil 0.3.13"
-/* generated using gnu compiler gcc (GCC) 4.1.3 20070929 (prerelease) (Ubuntu 4.1.2-16ubuntu2) */
-#define _STDINT_HAVE_STDINT_H 1
-#include <stdint.h>
-#endif
-#endif
