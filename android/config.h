5622527700ee91c2847b5f52b5c94bb5564c3478
Prajnashi S <prajnashi@gmail.com>
2009-03-03 18:23:39 +0100

    Add auto-generated headers

---
+++ android/config.h
@@ -0,0 +1,138 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define if compiling broken implementations */
+/* #undef ENABLE_BROKEN_IMPLS */
+
+/* Define if compiling new ABI */
+/* #undef ENABLE_NEW_ABI */
+
+/* Defined if host is amd64 */
+/* #undef HAVE_AMD64 */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define if compiler supports gcc inline assembly */
+#define HAVE_GCC_ASM 1
+
+/* Define if compiler supports PowerPC FPU instructions */
+/* #undef HAVE_GCC_ASM_POWERPC_FPU */
+
+/* Define to 1 if you have the `getpagesize' function. */
+#define HAVE_GETPAGESIZE 1
+
+/* Define to 1 if you have the `gettimeofday' function. */
+#define HAVE_GETTIMEOFDAY 1
+
+/* Defined if host is i386 */
+/* #undef HAVE_I386 */
+
+/* Define if ieee754.h exists */
+/* #define HAVE_IEEE754_H 1 */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define if lrint() is available */
+#define HAVE_LRINT 1
+
+/* Define if lrintf() is available */
+#define HAVE_LRINTF 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have a working `mmap' system call. */
+/* #undef HAVE_MMAP */
+
+/* Defined if host is powerpc */
+/* #undef HAVE_POWERPC */
+
+/* Define if rintf() is available */
+#define HAVE_RINTF 1
+
+/* Define to 1 if you have the `sigaction' function. */
+#define HAVE_SIGACTION 1
+
+/* Define if sincos() is available */
+/* #define HAVE_SINCOS 1 */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define if C symbols have leading underscore */
+/* #undef HAVE_SYMBOL_UNDERSCORE */
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* defined if unaligned memory access works correctly */
+/* #undef HAVE_UNALIGNED_ACCESS */
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Defined if prototype checking enabled */
+/* #undef LIBOIL_STRICT_PROTOTYPES */
+
+/* Name of package */
+#define PACKAGE "liboil"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "liboil"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "liboil 0.3.11"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "liboil"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.3.11"
+
+/* The size of `char', as computed by sizeof. */
+/* #undef SIZEOF_CHAR */
+
+/* The size of `int', as computed by sizeof. */
+/* #undef SIZEOF_INT */
+
+/* The size of `long', as computed by sizeof. */
+/* #undef SIZEOF_LONG */
+
+/* The size of `short', as computed by sizeof. */
+/* #undef SIZEOF_SHORT */
+
+/* The size of `void*', as computed by sizeof. */
+/* #undef SIZEOF_VOIDP */
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Defined if SSE functions need stack alignment wrappers */
+/* #undef USE_SSE_WRAPPER */
+
+/* Version number of package */
+#define VERSION "0.3.11"
+
+/* Define to 1 if your processor stores words with the most significant byte
+   first (like Motorola and SPARC, unlike Intel and VAX). */
+/* #undef WORDS_BIGENDIAN */
+
+/* have android */
+#define HAVE_ANDROID 1
